ca5362acedfeb71da8f2786787a98834a30988a6
Shahzain Aslam <Shahzainaslam11@outlook.com>
2026-04-10 04:33:34 +0500

    Update resampling.py

--- src/training/resampling.py
+++ src/training/resampling.py
@@ -1,1 +1,9 @@
+from imblearn.over_sampling import SMOTE
 
+def apply_smote(X, y, random_state=42):
+    """
+    Applying SMOTE resampling.
+    """
+    smote = SMOTE(random_state=random_state)
+    X_res, y_res = smote.fit_resample(X, y)
+    return X_res, y_res
